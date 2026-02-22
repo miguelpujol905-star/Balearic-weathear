bda6227bf4b7f51d7e7e66531d23868d770b1ff2
miguelpujol905-star <miguelpujol905@gmail.com>
2026-02-22 18:31:04 +0100

    Update api.js

--- api.js
+++ api.js
@@ -1,29 +1,23 @@
-//Api Palma
+// API Palma
 fetch("https://api.openweathermap.org/data/2.5/weather?lat=39.571111&lon=2.651667&appid=61a8f88ebfe982ef3e5a18707129382e&units=metric&lang=es")
-.then(response => response.json())
-.then(data => {
-document.getElementById("Temperatura-Palma").textContent = Math.round(data.main.temp);
-document.getElementById("Humedad-Palma").textContent = Math.round(data.main.humidity);
-})
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById("Temperatura-Palma").textContent = Math.round(data.main.temp);
+    document.getElementById("Humedad-Palma").textContent = Math.round(data.main.humidity);
+  });
 
 // API Menorca
 fetch("https://api.openweathermap.org/data/2.5/weather?lat=39.95&lon=4.11&appid=61a8f88ebfe982ef3e5a18707129382e&units=metric&lang=es")
-.then(response => response.json())
-.then(data => {
-  document.getElementById("Temperatura-Menorca").textContent = Math.round(data.main.temp);
-  document.getElementById("Humedad-Menorca").textContent = Math.round(data.main.humidity);
-});
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById("Temperatura-Menorca").textContent = Math.round(data.main.temp);
+    document.getElementById("Humedad-Menorca").textContent = Math.round(data.main.humidity);
+  });
 
 // API Ibiza
 fetch("https://api.openweathermap.org/data/2.5/weather?lat=38.98&lon=1.43&appid=61a8f88ebfe982ef3e5a18707129382e&units=metric&lang=es")
-.then(response => response.json())
-.then(data => {
-  // Mostrar temperatura y humedad
-  document.getElementById("Temperatura-Ibiza").textContent = Math.round(data.main.temp);
-  document.getElementById("Humedad-Ibiza").textContent = Math.round(data.main.humidity);
-
-
-
-
-
-
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById("Temperatura-Ibiza").textContent = Math.round(data.main.temp);
+    document.getElementById("Humedad-Ibiza").textContent = Math.round(data.main.humidity);
+  });
